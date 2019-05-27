a8f72ebdc37f339a69864ec767e65da27e149436
Filipe <filipereis96@ua.pt>
2019-05-27 01:31:30 +0100

    Network Sniffer v0.2

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # Who is Who
 
-Projecto TPR 2019<br>
+# About
+TPR Projecto 2019<br>
 Universidade de Aveiro
+Project where based on real-time traffic is given a profile with diferent permissions to internet content.
 
-#
-Filipe Reis - filipereis96@ua.pt
-Gabriel Patrício - gpatricio@ua.pt
+# Getting Started
+
+### sniffer.py
+`main` - Prints all the traffic captured
+
+output:
+```
+Ethernet Frame:
+	 - Destination : 7C:C3:85:7A:40:F7, Source: 60:57:18:17:10:FB, Protocol: 56710
+
+Ethernet Frame:
+	 - Destination : 00:00:00:00:00:00, Source: 00:00:00:00:00:00, Protocol: 8
+	 - IPv4 Packet:
+		 - Version: 4, Header Length: 20, TTL: 64
+		 - Protocol: 17, Source: 127.0.0.53, Target: 127.0.0.1
+	 - UDP segment: 
+		 - Source Port: 53, Destination Port: 58259, Length: 65293
+
+Ethernet Frame:
+	 - Destination : 7C:C3:85:7A:40:F7, Source: 60:57:18:17:10:FB, Protocol: 8
+	 - IPv4 Packet:
+		 - Version: 4, Header Length: 20, TTL: 64
+		 - Protocol: 6, Source: 192.168.1.70, Target: 216.58.201.164
+	 - TCP Segment:
+		 - Source Port: 40962, Destination Port: 443
+		 - Sequence: 3602270188, Acknowledgement: 3720628367
+		 - Flags: 
+			 - URG: 0, ACK: 1, PSH: 1, RST: 0, SYN: 0, FIN:0
+		 - Data: 
+		\x17\x03\x03\x00\x22\x2a\x51\x81\xd9\x61\xf7\x40\xa3\x56\x1e\x11\xd8\x52\x11
+		\x34\xae\x95\x04\xe9\x18\xc8\xdd\xaa\x40\x5e\x63\xe4\x4f\x3c\x3c\x36\xbd\xcd
+		\x7a
+
+...
+```
+
+
+## Autores
+* Filipe Reis - filipereis96@ua.pt
+* Gabriel Patrício - gpatricio@ua.pt
--- sniffer.py
+++ sniffer.py
@@ -3,15 +3,63 @@ import struct
 import textwrap
 
 def main():
+
+    TAB_1 ='\t - '
+    TAB_2 ='\t\t - '
+    TAB_3 ='\t\t\t - '    
+    TAB_4 ='\t\t\t\t - '
+
+    DATA_TAB_1 ='\t '
+    DATA_TAB_2 ='\t\t '    
+    DATA_TAB_3 ='\t\t\t '
+    DATA_TAB_4 ='\t\t\t\t '
+
     # Use AF_PACKET if on Linux/ AF_INET on Windows
-    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,  socket.IPPROTO_IP )
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,  socket.ntohs(3))
     
     while True:
-        raw_data, addr = conn.recvfrom(65536)
-        print("test")
+        raw_data, addr = conn.recvfrom(65535)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame:')
-        print('Destination : {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+        print(TAB_1 + 'Destination : {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+        
+        # 8 for IPv4
+        if eth_proto == 8:
+            version, header_length, ttl, proto, src, target, data = ipv4_packet(data)
+            print(TAB_1 + 'IPv4 Packet:')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl))
+            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
+
+            # ICMP
+            if proto == 1:
+                icmp_type, code , checksum, data = icmp_packet(data)
+                print(TAB_1 + 'ICMP Packet:')
+                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {}'.format(icmp_type, code, checksum))
+                print(TAB_2 + 'Data: ')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            # TCP
+            elif proto == 6:
+                src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data = tcp_segment(data)
+                print(TAB_1 + 'TCP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
+                print(TAB_2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence, acknowledgement))
+                print(TAB_2 + 'Flags: ')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN:{}'.format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+                print(TAB_2 + 'Data: ')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            # UDP
+            elif proto == 17:
+                src_port, dest_port, size, data = udp_segment(data)
+                print(TAB_1 + 'UDP segment: ')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port, size))
+
+            # Other
+            else:
+                print(TAB_1 + 'Data: ')
+                print(format_multi_line(DATA_TAB_2, data))
+
 
 
 # Unpack ethernet frame
@@ -23,5 +71,48 @@ def ethernet_frame(data):
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
+
+# Unpacks IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# Returns properly formatted IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str,addr)) 
+
+# Unpacks ICMP packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# Unpacks TCMP packet
+def tcp_segment(data):
+    src_port, dest_port, sequence, acknowledgement, offset_reserved_flags = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+# Unpacks UDP segment
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+# Formats multi-line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 main()
