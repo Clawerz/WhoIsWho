4ec73e2b38d54beb275fdafb4f717c89bb33c07a
Gabriel Patricio <gpatricio@ua.pt>
2019-05-28 22:48:29 +0100

    Plot multi dataset

--- ProfileClass.py
+++ ProfileClass.py
@@ -20,7 +20,7 @@ def waitforEnter(fstop=True):
                         input("Press ENTER to continue.")
 # 1
 # Function that does a plot based on a data array and a name
-def plot(data,name):
+def plot(data,name,data1,name1):
     plt.plot(data)
     plt.title(name)
     plt.show()
@@ -112,18 +112,29 @@ def extractFeaturesWavelet(data,scales=[2,4,8,16,32],Class=0):
 # Read data from files and plot the download + upload traffic
 # We don't need to verify if it is download or upload at least by now
 traffic_data = np.loadtxt(sys.argv[1])
+traffic_data2 = np.loadtxt(sys.argv[2])
+
 plt.figure(1)
-plot(traffic_data,'Plot Title')
+plot(traffic_data,'Plot1 Title',traffic_data2,'Plot2 Title')
 
 # 2
 # Divide each stream in observation windows of 5 minutes
 # Divide randomly a set for training and one for testing
 traffic_data_train, traffic_data_test= breakTrainTest(traffic_data)
+traffic_data_train2, traffic_data_test2= breakTrainTest(traffic_data2)
+
 plt.figure(2)
+plt.subplot(3,1,1)
 for i in range(2):
 	plt.plot(traffic_data_train[i],'b')
 	plt.plot(traffic_data_test[i],'g')
-plt.title('Adult')
+plt.title('Plot1')
+plt.ylabel('Bytes/sec')
+plt.subplot(3,1,2)
+for i in range(2):
+	plt.plot(traffic_data_train[i],'b')
+	plt.plot(traffic_data_test[i],'g')
+plt.title('Plot2')
 plt.ylabel('Bytes/sec')
 plt.show()
 waitforEnter()
@@ -131,8 +142,10 @@ waitforEnter()
 # 3
 # Extract and print features
 features_traffic, oClass_traffic = extractFeatures(traffic_data_train,Class=0)
-features= np.vstack(features_traffic)
-oClass= np.vstack(oClass_traffic)
+features_traffic2, oClass_traffic2 = extractFeatures(traffic_data_train,Class=1)
+
+features= np.vstack((features_traffic,features_traffic2))
+oClass= np.vstack((oClass_traffic,oClass_traffic2))
 print('\nTrain Stats Features Size:', features.shape)
 
 # 4
@@ -144,10 +157,11 @@ plotFeatures(features, oClass, 0, 1)
 # Extracts features from wavelet
 import scalogram
 scales=[2,4,8,16,32,64,128,256]
-features_kid_W,oClass_kid=extractFeaturesWavelet(traffic_data_train,scales,Class=0)
+features_plot1_W,oClass_plot1=extractFeaturesWavelet(traffic_data_train,scales,Class=0)
+features_plot2_W,oClass_plot2=extractFeaturesWavelet(traffic_data_train2,scales,Class=1)
 
-featuresW=np.vstack((features_kid_W))
-oClass=np.vstack((oClass_kid))
+featuresW=np.vstack((features_plot1_W,features_plot2_W))
+oClass=np.vstack((oClass_plot1,oClass_plot2))
 
 print('Train Wavelet Features Size:',featuresW.shape)
 plt.figure(7)
