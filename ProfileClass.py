95f3474ac88aebbcd904e82bcc0e6d7919633c82
Gabriel Patricio <gpatricio@ua.pt>
2019-05-29 01:25:06 +0100

    plots with third dataset

--- ProfileClass.py
+++ ProfileClass.py
@@ -20,18 +20,19 @@ def waitforEnter(fstop=True):
                         input("Press ENTER to continue.")
 # 1
 # Function that does a plot based on a data array and a name
-def plot(data,name,data1,name1):
+def plot(data,name,data1,name1,data2,name2):
 	plt.subplot(3,1,1)
 	plt.plot(data)
 	plt.title(name)
+	
 	plt.subplot(3,1,2)
 	plt.plot(data1)
 	plt.title(name1)
-	'''
+
 	plt.subplot(3,1,3)
 	plt.plot(data2)
 	plt.title(name2)
-	'''
+	
 	plt.show()
 	waitforEnter()
 
@@ -121,26 +122,25 @@ def distance(c,p):
 
 
 ################ Main Code #######################
-Classes={0:'Plot1',1:'Plot2'}
-#Classes={0:'Plot1',1:'Plot2',2:'Plot3'}
+
+Classes={0:'Plot1',1:'Plot2',2:'Plot3'}
 
 # 1
 # Read data from files and plot the download + upload traffic
 # We don't need to verify if it is download or upload at least by now
 traffic_data = np.loadtxt(sys.argv[1])
 traffic_data2 = np.loadtxt(sys.argv[2])
-#traffic_data3 = np.loadtxt(sys.argv[2])
+traffic_data3 = np.loadtxt(sys.argv[3])
 
 plt.figure(1)
-plot(traffic_data,'Plot1 Title',traffic_data2,'Plot2 Title')
-#plot(traffic_data,'Plot1 Title',traffic_data2,'Plot2 Title',traffic_data3,'Plot3 Title')
+plot(traffic_data,'Plot1 Title',traffic_data2,'Plot2 Title',traffic_data3,'Plot2 Title')
 
 # 2
 # Divide each stream in observation windows of 5 minutes
 # Divide randomly a set for training and one for testing
 traffic_data_train, traffic_data_test= breakTrainTest(traffic_data)
 traffic_data_train2, traffic_data_test2= breakTrainTest(traffic_data2)
-#traffic_data_train3, traffic_data_test3= breakTrainTest(traffic_data3)
+traffic_data_train3, traffic_data_test3= breakTrainTest(traffic_data3)
 
 plt.figure(2)
 plt.subplot(3,1,1)
@@ -149,36 +149,32 @@ for i in range(2):
 	plt.plot(traffic_data_test[i],'g')
 plt.title('Plot1')
 plt.ylabel('Bytes/sec')
+
 plt.subplot(3,1,2)
 for i in range(2):
 	plt.plot(traffic_data_train2[i],'b')
 	plt.plot(traffic_data_test2[i],'g')
 plt.title('Plot2')
 plt.ylabel('Bytes/sec')
-plt.show()
-'''
+
 plt.subplot(3,1,3)
 for i in range(2):
 	plt.plot(traffic_data_train3[i],'b')
 	plt.plot(traffic_data_test3[i],'g')
-plt.title('Plot2')
+plt.title('Plot3')
 plt.ylabel('Bytes/sec')
+
 plt.show()
-'''
 waitforEnter()
 
 # 3
 # Extract and print features
 features_traffic, oClass_traffic = extractFeatures(traffic_data_train,Class=0)
 features_traffic2, oClass_traffic2 = extractFeatures(traffic_data_train2,Class=1)
-#features_traffic3, oClass_traffic3 = extractFeatures(traffic_data_train3,Class=2)
+features_traffic3, oClass_traffic3 = extractFeatures(traffic_data_train3,Class=2)
 
-features= np.vstack((features_traffic,features_traffic2))
-oClass= np.vstack((oClass_traffic,oClass_traffic2))
-'''
 features= np.vstack((features_traffic,features_traffic2, features_traffic3))
 oClass= np.vstack((oClass_traffic,oClass_traffic2, oClass_traffic3))
-'''
 print('\nTrain Stats Features Size:', features.shape)
 
 # 4
@@ -192,14 +188,12 @@ import scalogram
 scales=[2,4,8,16,32,64,128,256]
 features_plot1_W,oClass_plot1 = extractFeaturesWavelet(traffic_data_train,scales,Class=0)
 features_plot2_W,oClass_plot2 = extractFeaturesWavelet(traffic_data_train2,scales,Class=1)
-#features_plot3_W,oClass_plot3 = extractFeaturesWavelet(traffic_data_train3,scales,Class=2)
+features_plot3_W,oClass_plot3 = extractFeaturesWavelet(traffic_data_train3,scales,Class=2)
 
-featuresW=np.vstack((features_plot1_W,features_plot2_W))
-oClass=np.vstack((oClass_plot1,oClass_plot2))
-'''
-featuresW=np.vstack((features_plot1_W,features_plot2_W,features_plot3_W)))
+
+featuresW=np.vstack((features_plot1_W,features_plot2_W,features_plot3_W))
 oClass=np.vstack((oClass_plot1,oClass_plot2,oClass_plot3))
-'''
+
 
 print('Train Wavelet Features Size:',featuresW.shape)
 plt.figure(7)
@@ -242,15 +236,13 @@ print('All Features Centroids:\n',centroids)
 
 traffic_data_test,oClass_traffic = extractFeatures(traffic_data_test,Class=0)
 traffic_data_test2,oClass_traffic2 = extractFeatures(traffic_data_test2,Class=1)
-#traffic_data_test3,oClass_traffic3 = extractFeatures(traffic_data_test3,Class=2)
-testFeatures=np.vstack((traffic_data_test,traffic_data_test2))
-#testFeatures=np.vstack((traffic_data_test,traffic_data_test2,traffic_data_test3))
+traffic_data_test3,oClass_traffic3 = extractFeatures(traffic_data_test3,Class=2)
+testFeatures=np.vstack((traffic_data_test,traffic_data_test2,traffic_data_test3))
 
 features_plot1_W,oClass_plot1=extractFeaturesWavelet(features_plot1_W,scales,Class=0)
 features_plot2_W,oClass_plot2=extractFeaturesWavelet(features_plot2_W,scales,Class=1)
-#features_plot3_W,oClass_plot3=extractFeaturesWavelet(features_plot3_W,scales,Class=2)
-testFeaturesW=np.vstack((features_plot1_W,features_plot2_W))
-#testFeaturesW=np.vstack((features_plot1_W,features_plot2_W,features_plot3_W))
+features_plot3_W,oClass_plot3=extractFeaturesWavelet(features_plot3_W,scales,Class=2)
+testFeaturesW=np.vstack((features_plot1_W,features_plot2_W,features_plot3_W))
 
 alltestFeatures=np.hstack((testFeatures,testFeaturesW))
 print('Test Features Size:', alltestFeatures.shape)
@@ -260,37 +252,34 @@ print('\n-- Classification based on Distances --')
 nObsTest,nFea=alltestFeatures.shape
 for i in range(nObsTest):
 	x=alltestFeatures[i]
-	dists=[distance(x,centroids[0]),distance(x,centroids[1])]
-	#dists=[distance(x,centroids[0]),distance(x,centroids[1]),,distance(x,centroids[2])]
+	dists=[distance(x,centroids[0]),distance(x,centroids[1]),distance(x,centroids[2])]
 	ndists=dists/np.sum(dists)
 	testClass=np.argsort(dists)[0]
 	
-	print('Obs: {:2}: Normalized Distances to Centroids: [{:.4f},{:.4f}] -> Classification: {} -> {}'.format(i,*ndists,testClass,Classes[testClass]))
-	#print('Obs: {:2}: Normalized Distances to Centroids: [{:.4f},{:.4f},{:.4f}] -> Classification: {} -> {}'.format(i,*ndists,testClass,Classes[testClass]))
+	print('Obs: {:2}: Normalized Distances to Centroids: [{:.4f},{:.4f},{:.4f}] -> Classification: {} -> {}'.format(i,*ndists,testClass,Classes[testClass]))
 
 # 12
 # Classification based on highest probability 
-from scipy.stats import multivariate_normal
-print('\n-- Classification based on Multivariate PDF (PCA Features) --')
-means={}
-for c in range(2):
-	pClass=(oClass==c).flatten()
-	means.update({c:np.mean(pcaFeatures[pClass,:],axis=0)})
+#from scipy.stats import multivariate_normal
+#print('\n-- Classification based on Multivariate PDF (PCA Features) --')
+#means={}
+#for c in range(2):
+#	pClass=(oClass==c).flatten()
+#	means.update({c:np.mean(pcaFeatures[pClass,:],axis=0)})
 #print(means)
 
-covs={}
-for c in range(2):
-	pClass=(oClass==c).flatten()
-	covs.update({c:np.cov(pcaFeatures[pClass,:],rowvar=0)})
+#covs={}
+#for c in range(2):
+#	pClass=(oClass==c).flatten()
+#	covs.update({c:np.cov(pcaFeatures[pClass,:],rowvar=0)})
 #print(covs)
 
-testpcaFeatures=pca.transform(alltestFeatures)	#uses pca fitted above, only transforms test data
-print(testpcaFeatures)
-nObsTest,nFea=testpcaFeatures.shape
-for i in range(nObsTest):
-	x=testpcaFeatures[i,:]
-	probs=np.array([multivariate_normal.pdf(x,means[0],covs[0]),multivariate_normal.pdf(x,means[1],covs[1])])
+#testpcaFeatures=pca.transform(alltestFeatures)	#uses pca fitted above, only transforms test data
+#print(testpcaFeatures)
+#nObsTest,nFea=testpcaFeatures.shape
+#for i in range(nObsTest):
+#	x=testpcaFeatures[i,:]
 	#probs=np.array([multivariate_normal.pdf(x,means[0],covs[0]),multivariate_normal.pdf(x,means[1],covs[1]),multivariate_normal.pdf(x,means[2],covs[2])])
-	testClass=np.argsort(probs)[-1]
+#	testClass=np.argsort(probs)[-1]
 	
-	print('Obs: {:2}: Probabilities: [{:.4e},{:.4e}] -> Classification: {} -> {}'.format(i,*probs,testClass,Classes[testClass]))
+#	print('Obs: {:2}: Probabilities: [{:.4e},{:.4e}] -> Classification: {} -> {}'.format(i,*probs,testClass,Classes[testClass]))
