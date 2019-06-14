bcf76fb1c8184883bc8d63e8aec3b64f9f704c0b
Filipe <filipereis96@ua.pt>
2019-06-14 23:38:32 +0100

    Kid dataset, and new functions added

--- ProfileClass.py
+++ ProfileClass.py
@@ -97,10 +97,50 @@ def plotFeatures(features,oClass,f1index=0,f2index=1):
 
 	colors=['b','g','r']
 	for i in range(nObs):
-		plt.plot(features[i],'o'+colors[int(oClass[i])])
+		plt.plot(features[i,f1index],features[i,f2index],'o'+colors[int(oClass[i])])
+	plt.show()
+	waitforEnter()
+
+def logplotFeatures(features,oClass,f1index=0,f2index=1):
+	nObs,nFea=features.shape
+	colors=['b','g','r']
+	for i in range(nObs):
+		plt.loglog(features[i,f1index],features[i,f2index],'o'+colors[int(oClass[i])])
 
 	plt.show()
 	waitforEnter()
+
+## -- 5 -- ##
+def extratctSilence(data,threshold=256):
+	if(data[0]<=threshold):
+		s=[1]
+	else:
+		s=[]
+	for i in range(1,len(data)):
+		if(data[i-1]>threshold and data[i]<=threshold):
+			s.append(1)
+		elif (data[i-1]<=threshold and data[i]<=threshold):
+			s[-1]+=1
+	
+	return(s)
+	
+def extractFeaturesSilence(data,Class=0):
+	features=[]
+	nObs,nSamp,nCols=data.shape
+	oClass=np.ones((nObs,1))*Class
+	for i in range(nObs):
+		silence_features=np.array([])
+		for c in range(nCols):
+			silence=extratctSilence(data[i,:,c],threshold=0)
+			if len(silence)>0:
+				silence_features=np.append(silence_features,[np.mean(silence),np.var(silence)])
+			else:
+				silence_features=np.append(silence_features,[0,0])
+			
+			
+		features.append(silence_features)
+		
+	return(np.array(features),oClass)
 
 # 7
 # Extracts features from wavelet
@@ -125,7 +165,7 @@ def distance(c,p):
 
 ################ Main Code #######################
 
-Classes={0:'Adult',1:'Kid',2:'Teenager'}
+Classes={0:'Kid',1:'Teenager',2:'Adult'}
 
 # 1
 # Read data from files and plot the download + upload traffic
@@ -135,7 +175,7 @@ traffic_data2 = np.loadtxt(sys.argv[2])
 traffic_data3 = np.loadtxt(sys.argv[3])
 
 plt.figure(1)
-plot(traffic_data,'Adult',traffic_data2,'Kid',traffic_data3,'Teenager')
+plot(traffic_data,'Kid',traffic_data2,'Teenager',traffic_data3,'Adult')
 
 # 2
 # Divide each stream in observation windows of 5 minutes
@@ -147,22 +187,22 @@ traffic_data_train3, traffic_data_test3= breakTrainTest(traffic_data3)
 plt.figure(2)
 plt.subplot(3,1,1)
 for i in range(2):
-	plt.plot(traffic_data_train[i],'b')
-	plt.plot(traffic_data_test[i],'g')
+	plt.plot(traffic_data_train[i,:,0],'b')
+	plt.plot(traffic_data_test[i,:,1],'g')
 plt.title('Adult')
 plt.ylabel('Bytes/sec')
 
 plt.subplot(3,1,2)
 for i in range(2):
-	plt.plot(traffic_data_train2[i],'b')
-	plt.plot(traffic_data_test2[i],'g')
+	plt.plot(traffic_data_train2[i,:,0],'b')
+	plt.plot(traffic_data_test2[i,:,1],'g')
 plt.title('Kid')
 plt.ylabel('Bytes/sec')
 
 plt.subplot(3,1,3)
 for i in range(2):
-	plt.plot(traffic_data_train3[i],'b')
-	plt.plot(traffic_data_test3[i],'g')
+	plt.plot(traffic_data_train3[i,:,0],'b')
+	plt.plot(traffic_data_test3[i,:,1],'g')
 plt.title('Teenager')
 plt.ylabel('Bytes/sec')
 
@@ -175,8 +215,8 @@ features_traffic, oClass_traffic = extractFeatures(traffic_data_train,Class=0)
 features_traffic2, oClass_traffic2 = extractFeatures(traffic_data_train2,Class=1)
 features_traffic3, oClass_traffic3 = extractFeatures(traffic_data_train3,Class=2)
 
-features= np.vstack((features_traffic,features_traffic2, features_traffic3))
-oClass= np.vstack((oClass_traffic,oClass_traffic2, oClass_traffic3))
+features= np.vstack((features_traffic, features_traffic2, features_traffic3))
+oClass= np.vstack((oClass_traffic, oClass_traffic2, oClass_traffic3))
 print('\nTrain Stats Features Size:', features.shape)
 
 # 4
@@ -236,10 +276,10 @@ for c in range(3):
 	centroids.update({c:np.mean(allFeatures[pClass,:],axis=0)})
 print('All Features Centroids:\n',centroids)
 
-traffic_data_testFeatures,oClass_traffic = extractFeatures(traffic_data_test,Class=0)
-traffic_data_test2Features,oClass_traffic2 = extractFeatures(traffic_data_test2,Class=1)
-traffic_data_test3Features,oClass_traffic3 = extractFeatures(traffic_data_test3,Class=2)
-testFeatures=np.vstack((traffic_data_test,traffic_data_test2,traffic_data_test3))
+features_traffic,oClass_traffic = extractFeatures(traffic_data_test,Class=0)
+features_traffic2,oClass_traffic2 = extractFeatures(traffic_data_test2,Class=1)
+features_traffic3,oClass_traffic3 = extractFeatures(traffic_data_test3,Class=2)
+testFeatures=np.vstack((features_traffic,features_traffic2,features_traffic3))
 
 features_plot1_W,oClass_plot1=extractFeaturesWavelet(traffic_data_test,scales,Class=0)
 features_plot2_W,oClass_plot2=extractFeaturesWavelet(traffic_data_test2,scales,Class=1)
@@ -286,3 +326,117 @@ for i in range(nObsTest):
 	testClass=np.argsort(probs)[-1]
 	
 	print('Obs: {:2}: Probabilities: [{:.4e},{:.4e}, {:.4e}] -> Classification: {} -> {}'.format(i,*probs,testClass,Classes[testClass]))
+
+## -- 13 -- ##
+scaler=StandardScaler()
+NormAllFeatures=scaler.fit_transform(allFeatures)
+
+NormAllTestFeatures=scaler.fit_transform(alltestFeatures)
+
+pca = PCA(n_components=3, svd_solver='full')
+NormPcaFeatures = pca.fit(NormAllFeatures).transform(NormAllFeatures)
+
+NormTestPcaFeatures = pca.fit(NormAllTestFeatures).transform(NormAllTestFeatures)
+
+##
+
+print('\n-- Classification based on Clustering (Kmeans) --')
+from sklearn.cluster import KMeans
+#K-means assuming 3 clusters
+centroids=np.array([])
+for c in range(3):
+	pClass=(oClass==c).flatten()
+	centroids=np.append(centroids,np.mean(NormPcaFeatures[pClass,:],axis=0))
+centroids=centroids.reshape((3,3))
+print('PCA (pcaFeatures) Centroids:\n',centroids)
+
+kmeans = KMeans(init=centroids, n_clusters=3)
+kmeans.fit(NormPcaFeatures)
+labels=kmeans.labels_
+print('Labels:',labels)
+
+#Determines and quantifies the presence of each original class observation in each cluster
+KMclass=np.zeros((3,3))
+for cluster in range(3):
+	p=(labels==cluster)
+	aux=oClass[p]
+	for c in range(3):
+		KMclass[cluster,c]=np.sum(aux==c)
+
+probKMclass=KMclass/np.sum(KMclass,axis=1)[:,np.newaxis]
+print(probKMclass)
+nObsTest,nFea=NormTestPcaFeatures.shape
+for i in range(nObsTest):
+	x=NormTestPcaFeatures[i,:].reshape((1,nFea))
+	label=kmeans.predict(x)
+	testClass=100*probKMclass[label,:].flatten()
+	print('Obs: {:2}: Probabilities beeing in each class: [{:.2f}%,{:.2f}%,{:.2f}%]'.format(i,*testClass))
+
+
+
+## -- 14 -- ##
+from sklearn.cluster import DBSCAN
+#DBSCAN assuming a neighborhood maximum distance of 1e11
+dbscan = DBSCAN(eps=10000)
+dbscan.fit(pcaFeatures)
+labels=dbscan.labels_
+print('Labels:',labels)
+
+## -- 15 -- #
+from sklearn import svm
+print('\n-- Classification based on Support Vector Machines --')
+svc = svm.SVC(kernel='linear').fit(NormAllFeatures, oClass)  
+rbf_svc = svm.SVC(kernel='rbf').fit(NormAllFeatures, oClass)  
+poly_svc = svm.SVC(kernel='poly',degree=2).fit(NormAllFeatures, oClass)  
+lin_svc = svm.LinearSVC().fit(NormAllFeatures, oClass)  
+
+L1=svc.predict(NormAllTestFeatures)
+print('class (from test PCA features with SVC):',L1)
+L2=rbf_svc.predict(NormAllTestFeatures)
+print('class (from test PCA features with Kernel RBF):',L2)
+L3=poly_svc.predict(NormAllTestFeatures)
+print('class (from test PCA features with Kernel poly):',L3)
+L4=lin_svc.predict(NormAllTestFeatures)
+print('class (from test PCA features with Linear SVC):',L4)
+print('\n')
+
+nObsTest,nFea=NormAllTestFeatures.shape
+for i in range(nObsTest):
+	print('Obs: {:2}: SVC->{} | Kernel RBF->{} | Kernel Poly->{} | LinearSVC->{}'.format(i,Classes[L1[i]],Classes[L2[i]],Classes[L3[i]],Classes[L4[i]]))
+
+## -- 16 -- #
+print('\n-- Classification based on Support Vector Machines  (PCA Features) --')
+svc = svm.SVC(kernel='linear').fit(NormPcaFeatures, oClass)  
+rbf_svc = svm.SVC(kernel='rbf').fit(NormPcaFeatures, oClass)  
+poly_svc = svm.SVC(kernel='poly',degree=2).fit(NormPcaFeatures, oClass)  
+lin_svc = svm.LinearSVC().fit(NormPcaFeatures, oClass)  
+
+L1=svc.predict(NormTestPcaFeatures)
+print('class (from test PCA features with SVC):',L1)
+L2=rbf_svc.predict(NormTestPcaFeatures)
+print('class (from test PCA features with Kernel RBF):',L2)
+L3=poly_svc.predict(NormTestPcaFeatures)
+print('class (from test PCA features with Kernel poly):',L3)
+L4=lin_svc.predict(NormTestPcaFeatures)
+print('class (from test PCA features with Linear SVC):',L4)
+print('\n')
+
+nObsTest,nFea=NormTestPcaFeatures.shape
+for i in range(nObsTest):
+	print('Obs: {:2}: SVC->{} | Kernel RBF->{} | Kernel Poly->{} | LinearSVC->{}'.format(i,Classes[L1[i]],Classes[L2[i]],Classes[L3[i]],Classes[L4[i]]))
+
+## -- 17 -- ##
+from sklearn.neural_network import MLPClassifier
+print('\n-- Classification based on Neural Networks --')
+
+alpha=1
+max_iter=100000
+clf = MLPClassifier(solver='lbfgs',alpha=alpha,hidden_layer_sizes=(100,),max_iter=max_iter)
+clf.fit(NormPcaFeatures, oClass) 
+LT=clf.predict(NormTestPcaFeatures) 
+print('class (from test PCA):',LT)
+
+nObsTest,nFea=NormTestPcaFeatures.shape
+for i in range(nObsTest):
+	print('Obs: {:2}: Classification->{}'.format(i,Classes[LT[i]]))
+
